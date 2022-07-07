995160db45aff986606a2db81c8d6aaaaa5cfa6e
Gabriel Maes <gabrielmaes@hotmail.com>
2022-07-07 19:59:12 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function sendEmail() {
 var Fname = window.document.getElementById("fname").value 
 var Lname = window.document.getElementById("lname").value
@@ -9,7 +8,7 @@ var Msg = window.document.getElementById("message").value
 if (Fname.length < 2 || Lname.length < 2 || Email.length < 7 || Subject.length < 2 || Msg.length < 2) {
     window.alert('Please verify your information and try again.')
 } else {
-    window.alert('Your message was successfully sent and I will get back to you ASAP.')
+    
 }
 }
 
